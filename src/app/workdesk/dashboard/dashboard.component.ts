1d32f51acb8007607f98a6669bf211d82ae6dfff
Muhammad Ahsan <csahsan0021@gmail.com>
2021-11-11 13:15:04 +0500

    workdesk udpated

--- src/app/workdesk/dashboard/dashboard.component.ts
+++ src/app/workdesk/dashboard/dashboard.component.ts
@@ -995,9 +995,9 @@ createoschart()
          ctx.lineTo(x + width - radius, y);
          ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
          ctx.lineTo(x + width, y + height - radius);
-         ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+         ctx.quadraticCurveTo(x + width, y + height, x + width - 0, y + height);
          ctx.lineTo(x + radius, y + height);
-         ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+         ctx.quadraticCurveTo(x, y + height, x, y + height - 0);
          ctx.lineTo(x, y + radius);
          ctx.quadraticCurveTo(x, y, x + radius, y);
  
